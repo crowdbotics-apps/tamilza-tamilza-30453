4567cba34d0a131e2bd8db13ce3442c7a1472f08
Crowdbotics <team@crowdbotics.com>
2021-09-11 00:59:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tamilza_tamilza_30453Identifier",
+  url: "https://tamilza_tamilza_30453Identifier.botics.co",
+  api: "https://tamilza_tamilza_30453Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
